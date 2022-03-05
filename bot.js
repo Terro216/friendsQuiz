10997237943d945040f5c271d81322b1c99fd4fb
ilyamed <0ilya@bk.ru>
2022-03-05 13:47:47 +0300

    vscode online test

--- bot.js
+++ bot.js
@@ -9,7 +9,6 @@ bot.onText(/\/echo (.+)/, (msg, match) => {
 	// 'msg' is the received Message from Telegram
 	// 'match' is the result of executing the regexp above on the text content
 	// of the message
-
 	const chatId = msg.chat.id
 	const resp = match[1] // the captured "whatever"
 
